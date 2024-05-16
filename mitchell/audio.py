5c802c4936b097c9702119376d4451109fb8dffa
vanwagom <vanwagom@oregonstate.edu>
2024-05-16 11:27:40 -0700

    Update audio.py

--- mitchell/audio.py
+++ mitchell/audio.py
@@ -1,14 +1,10 @@
 import pygame
 
 #from playsound import playsound
-
-#
-
-#file = 'birds'
-
-#
 #playsound(f'/home/ubuntu/pupper7/mitchell/sounds/{file}.mp3')
 
+
+file = 'birds'
 
 pygame.mixer.init()
 pygame.mixer.music.load(f'/home/ubuntu/pupper7/mitchell/sounds/{file}.mp3')
