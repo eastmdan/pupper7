0c2614975d5a851e27af6b9b8fb94d3eadc2ead0
vanwagom <vanwagom@oregonstate.edu>
2024-05-29 12:24:18 -0700

    functionize scripts

--- mitchell/findDevice.py
+++ mitchell/findDevice.py
@@ -1,13 +1,15 @@
 import pyaudio
 
-p = pyaudio.PyAudio()
-for i in range(p.get_device_count()):
-    info = p.get_device_info_by_index(i)
-    print(f"Device {i}: {info['name']} - Input Channels: {info['maxInputChannels']} - Output Channels: {info['maxOutputChannels']}")
-p.terminate()
+def findSpeakerDevice():
+    p = pyaudio.PyAudio()
+    for i in range(p.get_device_count()):
+        info = p.get_device_info_by_index(i)
+        print(f"Device {i}: {info['name']} - Input Channels: {info['maxInputChannels']} - Output Channels: {info['maxOutputChannels']}")
+    p.terminate()
 
 
-
+if __name__ == "__main__":
+    findSpeakerDevice()
 
 
 
