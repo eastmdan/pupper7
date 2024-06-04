3a091e23cef29e296507060d033b725d366d8bf4
vanwagom <vanwagom@oregonstate.edu>
2024-06-04 10:57:28 -0700

    Update movement.py

--- mitchell/movement.py
+++ mitchell/movement.py
@@ -7,37 +7,22 @@ drive_pub = Publisher(8830)
 arm_pub = Publisher(8410)
 
 
-MODES = Enum('MODES', 'SAFE DRIVE ARM')
+def activate():
+    drive_pub.send({"L1": 1, 
+            "R1": 0, 
+            "x": 0, 
+            "circle": 0, 
+            "triangle": 0, 
+            "L2": 0, 
+            "R2": 0, 
+            "ly": 0, 
+            "lx": 0, 
+            "rx": 0, 
+            "message_rate": 20, 
+            "ry": 0, 
+            "dpady": 0, 
+            "dpadx": 0})
+    
+    
 
-mode = MODES.DRIVE
-
-l_trigger = 100
-forward = 100
-twist = 100
-
-while True:
-    if mode == MODES.DRIVE:
-        forward_left  = 100
-        forward_right = 100
-        twist = 0
-
-        on_right = 0
-        on_left = 0
-        l_trigger = 0
-
-        if on_left or on_right:
-            if on_right:
-                forward = forward_right
-            else:
-                forward = forward_left
-
-            slow = 150
-            fast = 500
-
-            max_speed = (fast+slow)/2 + l_trigger*(fast-slow)/2
-
-            out = {'f':(max_speed*forward),'t':-150*twist}
-            drive_pub.send(out)
-            print(out)
-        else:
-            drive_pub.send({'f':0,'t':0})
+activate()
