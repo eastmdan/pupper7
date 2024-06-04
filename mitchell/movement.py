540c8a7e6213e7f82a123e50c8acd55d6d2b5f68
vanwagom <vanwagom@oregonstate.edu>
2024-06-04 11:20:39 -0700

    Update movement.py

--- mitchell/movement.py
+++ mitchell/movement.py
@@ -6,22 +6,20 @@ drive_pub = Publisher(8830)
 arm_pub = Publisher(8410)
 
 
-def init():
-    print('init')
-    drive_pub.send({"L1": 0, 
-            "R1": 0, 
-            "x": 0, 
-            "circle": 0, 
-            "triangle": 0, 
-            "L2": 0, 
-            "R2": 0, 
-            "ly": 0, 
-            "lx": 0, 
-            "rx": 0, 
-            "message_rate": 20, 
-            "ry": 0, 
-            "dpady": 0, 
-            "dpadx": 0})
+forward  = - 50
+twist = 0
+
+l_trigger = 50
+
+
+slow = 150
+fast = 500
+
+max_speed = (fast+slow)/2 + l_trigger*(fast-slow)/2
+out = {'f':(max_speed*forward),'t':-150*twist}
+drive_pub.send(out)
+print(out)
+
 
 def activate():
     print('activate')
@@ -66,20 +64,38 @@ def move():
             "triangle": 0, 
             "L2": 0, 
             "R2": 0, 
-            "ly": 1, 
+            "ly": -1, 
             "lx": 0, 
             "rx": 0, 
             "message_rate": 20, 
             "ry": 0, 
             "dpady": 0, 
-            "dpadx": 0})   
+            "dpadx": 0}) 
+    
+    
+def stop():
+    print('trot')
+    drive_pub.send({"L1": 0, 
+            "R1": 1, 
+            "x": 0, 
+            "circle": 0, 
+            "triangle": 0, 
+            "L2": 0, 
+            "R2": 0, 
+            "ly": 0, 
+            "lx": 0, 
+            "rx": 0, 
+            "message_rate": 20, 
+            "ry": 0, 
+            "dpady": 0, 
+            "dpadx": 0}) 
 
 
-time.sleep(1)
-init()
 time.sleep(1)
 activate()
 time.sleep(1)
 trot()
 time.sleep(1)
 move()
+time.sleep(5)
+stop()
