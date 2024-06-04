dc2c15fdd2f6ca40fb30f2683ca2287d04fc7919
vanwagom <vanwagom@oregonstate.edu>
2024-06-04 16:00:47 -0700

    Update movement.py

--- mitchell/movement.py
+++ mitchell/movement.py
@@ -28,77 +28,20 @@ def activate():
     print('activate')
     drive_pub.send({
         "L1": 1, 
-        "R1": 0, 
-        "x": 0, 
-        "circle": 0, 
-        "triangle": 0, 
-        "L2": 0, 
-        "R2": 0, 
-        "ly": 0, 
-        "lx": 0, 
-        "rx": 0, 
-        "message_rate": 60, 
-        "ry": 0, 
-        "dpady": 0, 
-        "dpadx": 0
     })
 
 def trot():
     print('trot')
     drive_pub.send({
-        "L1": 0, 
         "R1": 1, 
-        "x": 0, 
-        "circle": 0, 
-        "triangle": 0, 
-        "L2": 0, 
-        "R2": 0, 
-        "ly": 0, 
-        "lx": 0, 
-        "rx": 0, 
-        "message_rate": 60, 
-        "ry": 0, 
-        "dpady": 0, 
-        "dpadx": 0
     })  
 
 def move():
     print('move')
     drive_pub.send({
-        "L1": 0, 
-        "R1": 0, 
-        "x": 0, 
-        "circle": 0, 
-        "triangle": 0, 
-        "L2": 0, 
-        "R2": 0, 
         "ly": 1, 
-        "lx": 0, 
-        "rx": 0, 
-        "message_rate": 60, 
-        "ry": 0, 
-        "dpady": 0, 
-        "dpadx": 0
     }) 
      
-def stop():
-    print('stop')
-    drive_pub.send({
-        "L1": 0, 
-        "R1": 1, 
-        "x": 0, 
-        "circle": 0, 
-        "triangle": 0, 
-        "L2": 0, 
-        "R2": 0, 
-        "ly": 0, 
-        "lx": 0, 
-        "rx": 0, 
-        "message_rate": 60, 
-        "ry": 0, 
-        "dpady": 0, 
-        "dpadx": 0
-    }) 
     
 
 
@@ -134,7 +77,7 @@ def moveForward(duration):
         time.sleep(0.3)
 
     time.sleep(0.3)
-    stop() # Stop trotting
+    trot() # Stop trotting
 
 
 def rotate(duration):
@@ -167,7 +110,7 @@ def rotate(duration):
         time.sleep(0.3)
     
     time.sleep(0.3)
-    stop() # Stop trotting
+    trot() # Stop trotting
 
 
 
@@ -177,4 +120,7 @@ time.sleep(1)
 activate()
 time.sleep(1)
 
+
 moveForward(3)
+time.sleep(1)
+rotate(3)
