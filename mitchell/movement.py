8d3b1726b276c16ffb1803009f03c151ca8191c0
vanwagom <vanwagom@oregonstate.edu>
2024-06-04 15:59:02 -0700

    Update movement.py

--- mitchell/movement.py
+++ mitchell/movement.py
@@ -46,9 +46,20 @@ def activate():
 def trot():
     print('trot')
     drive_pub.send({
-
+        "L1": 0, 
         "R1": 1, 
+        "x": 0, 
+        "circle": 0, 
+        "triangle": 0, 
+        "L2": 0, 
+        "R2": 0, 
+        "ly": 0, 
+        "lx": 0, 
+        "rx": 0, 
         "message_rate": 60, 
+        "ry": 0, 
+        "dpady": 0, 
+        "dpadx": 0
     })  
 
 def move():
