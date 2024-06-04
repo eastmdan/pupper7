63f1a7a242800bbca115a82cdacf9768025ffe81
vanwagom <vanwagom@oregonstate.edu>
2024-06-04 10:49:12 -0700

    Update movement.py

--- mitchell/movement.py
+++ mitchell/movement.py
@@ -7,8 +7,22 @@ drive_pub = Publisher(8830)
 arm_pub = Publisher(8410)
 
 
+MODES = Enum('MODES', 'SAFE DRIVE ARM')
+
+mode = MODES.DRIVE
+
+
 while True:
-    drive_pub.send({'f':0,'t':0})
-    time.sleep(1000)
-    drive_pub.send({'f':20,'t':0})
-    time.sleep(1000)
+    if mode == MODES.DRIVE:
+        slow = 150
+        fast = 500
+
+        max_speed = (fast+slow)/2 + l_trigger*(fast-slow)/2
+
+        out = {'f':(max_speed*forward),'t':-150*twist}
+        drive_pub.send(out)
+            
+        drive_pub.send({'f':0,'t':0})
+        time.sleep(1000)
+        drive_pub.send({'f':20,'t':0})
+        time.sleep(1000)
