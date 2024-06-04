b694fb40d1c1cfd51f8ee977e047c55de41cecca
vanwagom <vanwagom@oregonstate.edu>
2024-06-04 15:46:23 -0700

    Update movement.py

--- mitchell/movement.py
+++ mitchell/movement.py
@@ -5,91 +5,100 @@ import time
 drive_pub = Publisher(8830)
 
 
-
 def init():
     print('init')
-    drive_pub.send({"L1": 0, 
-            "R1": 0, 
-            "x": 0, 
-            "circle": 0, 
-            "triangle": 0, 
-            "L2": 0, 
-            "R2": 0, 
-            "ly": 0, 
-            "lx": 0, 
-            "rx": 0, 
-            "message_rate": 60, 
-            "ry": 0, 
-            "dpady": 0, 
-            "dpadx": 0})
+    drive_pub.send({
+        "L1": 0, 
+        "R1": 0, 
+        "x": 0, 
+        "circle": 0, 
+        "triangle": 0, 
+        "L2": 0, 
+        "R2": 0, 
+        "ly": 0, 
+        "lx": 0, 
+        "rx": 0, 
+        "message_rate": 60, 
+        "ry": 0, 
+        "dpady": 0, 
+        "dpadx": 0
+    })
 
 def activate():
     print('activate')
-    drive_pub.send({"L1": 1, 
-            "R1": 0, 
-            "x": 0, 
-            "circle": 0, 
-            "triangle": 0, 
-            "L2": 0, 
-            "R2": 0, 
-            "ly": 0, 
-            "lx": 0, 
-            "rx": 0, 
-            "message_rate": 60, 
-            "ry": 0, 
-            "dpady": 0, 
-            "dpadx": 0})
+    drive_pub.send({
+        "L1": 1, 
+        "R1": 0, 
+        "x": 0, 
+        "circle": 0, 
+        "triangle": 0, 
+        "L2": 0, 
+        "R2": 0, 
+        "ly": 0, 
+        "lx": 0, 
+        "rx": 0, 
+        "message_rate": 60, 
+        "ry": 0, 
+        "dpady": 0, 
+        "dpadx": 0
+    })
 
 def trot():
     print('trot')
-    drive_pub.send({"L1": 0, 
-            "R1": 1, 
-            "x": 0, 
-            "circle": 0, 
-            "triangle": 0, 
-            "L2": 0, 
-            "R2": 0, 
-            "ly": 0, 
-            "lx": 0, 
-            "rx": 0, 
-            "message_rate": 60, 
-            "ry": 0, 
-            "dpady": 0, 
-            "dpadx": 0})  
+    drive_pub.send({
+        "L1": 0, 
+        "R1": 1, 
+        "x": 0, 
+        "circle": 0, 
+        "triangle": 0, 
+        "L2": 0, 
+        "R2": 0, 
+        "ly": 0, 
+        "lx": 0, 
+        "rx": 0, 
+        "message_rate": 60, 
+        "ry": 0, 
+        "dpady": 0, 
+        "dpadx": 0
+    })  
 
 def move():
     print('move')
-    drive_pub.send({"L1": 0, 
-            "R1": 0, 
-            "x": 0, 
-            "circle": 0, 
-            "triangle": 0, 
-            "L2": 0, 
-            "R2": 0, 
-            "ly": 1, 
-            "lx": 0, 
-            "rx": 0, 
-            "message_rate": 60, 
-            "ry": 0, 
-            "dpady": 0, 
-            "dpadx": 0}) 
+    drive_pub.send({
+        "L1": 0, 
+        "R1": 0, 
+        "x": 0, 
+        "circle": 0, 
+        "triangle": 0, 
+        "L2": 0, 
+        "R2": 0, 
+        "ly": 1, 
+        "lx": 0, 
+        "rx": 0, 
+        "message_rate": 60, 
+        "ry": 0, 
+        "dpady": 0, 
+        "dpadx": 0
+    }) 
      
 def stop():
     print('stop')
-    drive_pub.send({"L1": 0, 
-            "R1": 1, 
-            "x": 0, 
-            "circle": 0, 
-            "triangle": 0, 
-            "L2": 0, 
-            "R2": 0, 
-            "ly": 0, 
-            "lx": 0, 
-            "rx": 0, 
-            "message_rate": 60, 
-            "ry": 0, 
-            "dpady": 0, 
-            "dpadx": 0}) 
+    drive_pub.send({
+        "L1": 0, 
+        "R1": 1, 
+        "x": 0, 
+        "circle": 0, 
+        "triangle": 0, 
+        "L2": 0, 
+        "R2": 0, 
+        "ly": 0, 
+        "lx": 0, 
+        "rx": 0, 
+        "message_rate": 60, 
+        "ry": 0, 
+        "dpady": 0, 
+        "dpadx": 0
+    }) 
     
 
 
@@ -98,13 +107,16 @@ def moveForward(duration):
     time.sleep(0.5)
     trot() # start trotting
 
+    time.sleep(1)
+
     print('Moving Forward')
 
     start_time = time.time() # start time keep
 
     # Loop until duration has passed
     while (time.time() - start_time) < duration:
-        drive_pub.send({"L1": 0, 
+        drive_pub.send({
+            "L1": 0, 
             "R1": 0, 
             "x": 0, 
             "circle": 0, 
@@ -117,7 +129,8 @@ def moveForward(duration):
             "message_rate": 60, 
             "ry": 0, 
             "dpady": 0, 
-            "dpadx": 0}) 
+            "dpadx": 0
+        }) 
         time.sleep(0.3)
 
     stop() # Stop trotting
@@ -134,7 +147,8 @@ def rotate(duration):
 
     # Loop until duration has passed
     while (time.time() - start_time) < duration:
-        drive_pub.send({"L1": 0, 
+        drive_pub.send({
+            "L1": 0, 
             "R1": 0, 
             "x": 0, 
             "circle": 0, 
@@ -147,17 +161,18 @@ def rotate(duration):
             "message_rate": 60, 
             "ry": 0, 
             "dpady": 0, 
-            "dpadx": 0}) 
+            "dpadx": 0
+        }) 
         time.sleep(0.3)
     
     stop() # Stop trotting
 
 
 
-time.sleep(0.5)
+time.sleep(1)
 init()
-time.sleep(0.5)
+time.sleep(1)
 activate()
-time.sleep(0.5)
+time.sleep(1)
 
 moveForward(5)
