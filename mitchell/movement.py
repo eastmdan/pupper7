8cde25a4f84422b70aa177dfaddd7b0dfb83af20
vanwagom <vanwagom@oregonstate.edu>
2024-06-04 11:11:59 -0700

    Update movement.py

--- mitchell/movement.py
+++ mitchell/movement.py
@@ -6,6 +6,22 @@ drive_pub = Publisher(8830)
 arm_pub = Publisher(8410)
 
 
+def init():
+    drive_pub.send({"L1": 0, 
+            "R1": 0, 
+            "x": 0, 
+            "circle": 0, 
+            "triangle": 0, 
+            "L2": 0, 
+            "R2": 0, 
+            "ly": 0, 
+            "lx": 0, 
+            "rx": 0, 
+            "message_rate": 20, 
+            "ry": 0, 
+            "dpady": 0, 
+            "dpadx": 0})
+
 def activate():
     drive_pub.send({"L1": 1, 
             "R1": 0, 
@@ -54,8 +70,12 @@ def move():
             "dpady": 0, 
             "dpadx": 0})   
 
+
+time.sleep(1000)
+init()
+time.sleep(1000)
 activate()
-time.sleep(5000)
+time.sleep(1000)
 trot()
-time.sleep(5000)
+time.sleep(1000)
 move()
