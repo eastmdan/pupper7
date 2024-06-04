2b355a38470cc9a29f78679b9f6f4de46379c1e5
vanwagom <vanwagom@oregonstate.edu>
2024-06-04 10:53:27 -0700

    Update movement.py

--- mitchell/movement.py
+++ mitchell/movement.py
@@ -11,18 +11,33 @@ MODES = Enum('MODES', 'SAFE DRIVE ARM')
 
 mode = MODES.DRIVE
 
+l_trigger = 100
+forward = 100
+twist = 100
 
 while True:
     if mode == MODES.DRIVE:
-        slow = 150
-        fast = 500
+        forward_left  = 100
+        forward_right = 100
+        twist = 0
 
-        max_speed = (fast+slow)/2 + 1*(fast-slow)/2
+        on_right = 0
+        on_left = 0
+        l_trigger = 0
 
-        out = {'f':(max_speed*1),'t':-150*1}
-        drive_pub.send(out)
-            
-        drive_pub.send({'f':0,'t':0})
-        time.sleep(1000)
-        drive_pub.send({'f':20,'t':0})
-        time.sleep(1000)
+        if on_left or on_right:
+            if on_right:
+                forward = forward_right
+            else:
+                forward = forward_left
+
+            slow = 150
+            fast = 500
+
+            max_speed = (fast+slow)/2 + l_trigger*(fast-slow)/2
+
+            out = {'f':(max_speed*forward),'t':-150*twist}
+            drive_pub.send(out)
+            print(out)
+        else:
+            drive_pub.send({'f':0,'t':0}
