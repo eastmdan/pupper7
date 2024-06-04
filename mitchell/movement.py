434007d865f6ea8dfdc7ff56e6bf923a6d627157
vanwagom <vanwagom@oregonstate.edu>
2024-06-04 11:03:07 -0700

    Update movement.py

--- mitchell/movement.py
+++ mitchell/movement.py
@@ -1,12 +1,46 @@
 from UDPComms import Publisher
+import time
+
 
 drive_pub = Publisher(8830)
 arm_pub = Publisher(8410)
 
 
 def activate():
+    drive_pub.send({"L1": 1, 
+            "R1": 0, 
+            "x": 0, 
+            "circle": 0, 
+            "triangle": 0, 
+            "L2": 0, 
+            "R2": 0, 
+            "ly": 0, 
+            "lx": 0, 
+            "rx": 0, 
+            "message_rate": 20, 
+            "ry": 0, 
+            "dpady": 0, 
+            "dpadx": 0})
+
+def trot():
     drive_pub.send({"L1": 0, 
             "R1": 1, 
+            "x": 0, 
+            "circle": 0, 
+            "triangle": 0, 
+            "L2": 0, 
+            "R2": 0, 
+            "ly": 0, 
+            "lx": 0, 
+            "rx": 0, 
+            "message_rate": 20, 
+            "ry": 0, 
+            "dpady": 0, 
+            "dpadx": 0})  
+
+def move():
+    drive_pub.send({"L1": 0, 
+            "R1": 0, 
             "x": 0, 
             "circle": 0, 
             "triangle": 0, 
@@ -18,8 +52,10 @@ def activate():
             "message_rate": 20, 
             "ry": 0, 
             "dpady": 0, 
-            "dpadx": 0})
-    
-    
+            "dpadx": 0})   
 
 activate()
+time.sleep(500)
+trot()
+time.sleep(500)
+move()
