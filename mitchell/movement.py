fe58d6a2fc9e5708dc1d741613e353c2acb8e4a3
vanwagom <vanwagom@oregonstate.edu>
2024-06-05 10:55:28 -0700

    update

--- mitchell/movement.py
+++ mitchell/movement.py
@@ -62,92 +62,95 @@ def trot():
         "dpadx": 0
     })  
 
-def move():
-    print('move')
-    drive_pub.send({
-        "L1": 0, 
-        "R1": 0, 
-        "x": 0, 
-        "circle": 0, 
-        "triangle": 0, 
-        "L2": 0, 
-        "R2": 0, 
-        "ly": 1, 
-        "lx": 0, 
-        "rx": 0, 
-        "message_rate": 60, 
-        "ry": 0, 
-        "dpady": 0, 
-        "dpadx": 0
-    }) 
 
-
-def moveForward(duration):
-
+def move(forward, lateral, duration):
     time.sleep(0.5)
-    trot() # start trotting
-
+    trot()  # Start trotting
     time.sleep(0.3)
-
     print('Moving Forward')
 
-    start_time = time.time() # start time keep
-
-    # Loop until duration has passed
-    while (time.time() - start_time) < duration:       
-        drive_pub.send({
-            "L1": 0, 
-            "R1": 0, 
-            "x": 0, 
-            "circle": 0, 
-            "triangle": 0, 
-            "L2": 0, 
-            "R2": 0, 
-            "ly": 1, 
-            "lx": 0.25, 
-            "rx": 0, 
-            "message_rate": 60, 
-            "ry": 0, 
-            "dpady": 0, 
-            "dpadx": 0
-        }) 
-        time.sleep(0.3)
-
-    time.sleep(0.3)
-    trot() # Stop trotting
-
-
-def rotate(duration):
-    
-    time.sleep(0.5)
-    trot() # start trotting
-
-    print('Rotate')
-
-    start_time = time.time() # start time keep
+    ramp_duration = 2  # time to accel to full speed
+    start_time = time.time()
 
     # Loop until duration has passed
     while (time.time() - start_time) < duration:
+        elapsed_time = time.time() - start_time
+        
+        # ramp up speed
+        if elapsed_time < ramp_duration:
+            ly = elapsed_time / ramp_duration  
+            lx = elapsed_time / ramp_duration
+        else:
+            # set to full speed after 2 seconds
+            ly = 1
+            lx = 1
+
         drive_pub.send({
-            "L1": 0, 
-            "R1": 0, 
-            "x": 0, 
-            "circle": 0, 
-            "triangle": 0, 
-            "L2": 0, 
-            "R2": 0, 
-            "ly": 0, 
-            "lx": 0, 
-            "rx": 1, 
-            "message_rate": 60, 
-            "ry": 0, 
-            "dpady": 0, 
+            "L1": 0,
+            "R1": 0,
+            "x": 0,
+            "circle": 0,
+            "triangle": 0,
+            "L2": 0,
+            "R2": 0,
+            "ly": forward*ly,
+            "lx": lateral*lx,
+            "rx": 0,
+            "message_rate": 60,
+            "ry": 0,
+            "dpady": 0,
             "dpadx": 0
-        }) 
-        time.sleep(0.3)
-    
+        })
+
+        time.sleep(0.3) # 0.016 based on message rate
+
     time.sleep(0.3)
-    trot() # Stop trotting
+    trot()  # Stop trotting
+
+
+def rotate(forward, lateral, duration):
+    time.sleep(0.5)
+    trot()  # Start trotting
+    time.sleep(0.3)
+    print('Moving Forward')
+
+    ramp_duration = 2  # time to accel to full speed
+    start_time = time.time()
+
+    # Loop until duration has passed
+    while (time.time() - start_time) < duration:
+        elapsed_time = time.time() - start_time
+        
+        # ramp up speed
+        if elapsed_time < ramp_duration:
+            ry = elapsed_time / ramp_duration  
+            rx = elapsed_time / ramp_duration
+        else:
+            # set to full speed after 2 seconds
+            ly = 1
+            lx = 1
+
+        drive_pub.send({
+            "L1": 0,
+            "R1": 0,
+            "x": 0,
+            "circle": 0,
+            "triangle": 0,
+            "L2": 0,
+            "R2": 0,
+            "ly": 0,
+            "lx": 0,
+            "rx": lateral*rx,
+            "message_rate": 60,
+            "ry": forward*ry,
+            "dpady": 0,
+            "dpadx": 0
+        })
+
+        time.sleep(0.3) # 0.016 based on message rate
+
+    time.sleep(0.3)
+    trot()  # Stop trotting
     
 
 time.sleep(1)
@@ -155,4 +158,4 @@ init()
 time.sleep(1)
 activate()
 time.sleep(1)
-moveForward(5)
+move(1,0.35,5)
