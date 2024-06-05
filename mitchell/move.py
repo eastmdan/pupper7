13e446b22ab100fbbf6b4a700833deed56a3b4b7
vanwagom <vanwagom@oregonstate.edu>
2024-06-05 13:08:00 -0700

    Update move.py

--- mitchell/move.py
+++ mitchell/move.py
@@ -31,20 +31,17 @@ throw_distance = 1. # m away form the box the robot will throw
 drive_pub = Publisher(8830)
 
 
-def move_robot(error_store, error_store_lock):
+def move_robot(error_store):
     """Move the robot based on continuously updated errors until a certain distance is reached."""
 
     # Movement parameters
     duration = interval
     scaling_factor = 0.5 #scaling of movement speeds
-    throw_distance = 1  # distance for launch
+    throw_distance = 0.25  # distance for launch
 
     while True:
     
-        with error_store_lock:
-            if not error_store:  # Check if the store is empty
-                continue  # You might want to sleep here to avoid tight looping
-            error_x, error_y, z_distance = error_store.pop(0) 
+        error_x, error_y, z_distance = error_store
             
 
         # Calculate normalized forward and lateral movements
@@ -55,7 +52,7 @@ def move_robot(error_store, error_store_lock):
         lateral = max(-1, min(1, scaling_factor * lateral_error_normalized))
         forward = max(-1, min(1, scaling_factor * forward_error_normalized))
 
-        if abs(z_distance - throw_distance) > 0:  # Movement command condition
+        if abs(z_distance - throw_distance) >= 0:  # Movement command condition
 
             ramp_duration = 1  # Time to accelerate to full speed
             start_time = time.time()
@@ -78,7 +75,7 @@ def move_robot(error_store, error_store_lock):
                     "message_rate": 60
                 })
 
-                time.sleep(0.016)  # Sleep time based on message rate
+                time.sleep(0.35)  # Sleep time based on message rate 0.016
 
         # Condition to exit loop (if required)
         else:   # If z-distance is within range, stop function
