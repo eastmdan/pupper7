ecf2add286860960283db5daec1bbb89f9c4084b
vanwagom <vanwagom@oregonstate.edu>
2024-06-05 18:50:41 -0700

    Update move_final2.py

--- mitchell/move_final2.py
+++ mitchell/move_final2.py
@@ -143,11 +143,13 @@ def main():
                 stop_trot = True
                 time.sleep(1)
             
-            rotate_robot(error_x+15,error_y,z,2)
-
+            rotate_robot(error_x+30,error_y,z,2)
+            
             os.system("echo 2000000 > /sys/class/pwm/pwmchip0/pwm3/duty_cycle")
             time.sleep(0.5)
             os.system("echo 500000 > /sys/class/pwm/pwmchip0/pwm3/duty_cycle")
+            
+            break
 
         
     
